ae164461cd4dbd960877cf9967da191afe4f4b32
cumali toprak <cumalitoprakk@gmail.com>
2020-09-26 14:13:08 +0300

    End of Section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';  //imported for the http request.
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //Ngx Bootstrap kullanırken bazı fonksionaliteler için gerekli.
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
 
 //node_modules'den gelen importları yukarı localden gelen importları aşağıya yazarız. Sadece tertip ve düzen için. Zorunlu değil.
 
@@ -13,20 +14,28 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
 
 @NgModule({
-  declarations: [				
+  declarations: [							
     AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
   imports: [
     BrowserModule,
     HttpClientModule, //imported for the http request.
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService, //yeni bir servis oluşturduğumuzda buraya eklemeliyiz. Böylece diğer componentlere inject edilmeye hazır olur.
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,39 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
-    //injection AuthService to this component:
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  //injection AuthService to this component:
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router //login ve logout olduktan sonra specific pagelere redirect olmak için bu servisi import ederiz.
+  ) {}
 
-  ngOnInit() {
-   
+  ngOnInit() {}
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        //to returning value is observable, we must always subscribe to observable.
+        this.alertify.success('Logged in successfully'); //parameters of the subscribe method is completely optional.
+      },
+      (error) => {
+        this.alertify.error(error);
+      },
+      () =>{
+        //login olduktan sonra: redirect to members
+        this.router.navigate(['/members']); //bunu hemen login olduktan sonra ilk parametrenin içinde de yapabilirdik. Fakat bu (complete) kısmında yapmak daha güzel.
+      }
+    );
   }
 
-
-  login(){
-    this.authService.login(this.model).subscribe(next => {   //to returning value is observable, we must always subscribe to observable.
-      this.alertify.success('Logged in successfully');   //parameters of the subscribe method is completely optional.
-    }, error => {
-      this.alertify.error(error);
-    })
-  }
-
-  loggedIn(){
+  loggedIn() {
     // const token = localStorage.getItem('token'); //local storagedan keyi token olan tokenı getirir.
     // return !!token; // if token empty ise false, dolu ise true döndürür.
     return this.authService.loggedIn();
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token'); // tokenı local storagedan atmak için.
-    this.alertify.message('logged out')
+    this.alertify.message('logged out');
+    this.router.navigate(['home']); // logout olduktan sonra: redirect to home
   }
-
 }
